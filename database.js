30f0a7d529a6953358afdcd4c59cd5d42b34887b
Greg-prog <olajideemmanuel250@gmail.com>
2024-11-30 04:30:15 +0100

    final project first commit

---
+++ database.js
@@ -0,0 +1,23 @@
+const mysql = require(`mysql2`);
+require(`dotenv`).config();
+
+
+const db = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    multipleStatements: true
+})
+
+db.getConnection((err, result)=>{
+    if(!err){
+        console.log(`Database Connected successfully`);
+        
+    }else(
+        console.log(err)
+        
+    )
+})
+
+module.exports = db
